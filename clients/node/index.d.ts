25edbecbd17c67b070144fb2ae1d74664c206d84
Hüseyin Karakaya <karakayahuseyin023@gmail.com>
2026-06-07 19:51:42 +0300

    feat(npm): add TypeScript declaration file index.d.ts to node client package

---
+++ clients/node/index.d.ts
@@ -0,0 +1,13 @@
+/**
+ * TypeScript declaration file for @setuav/mavlink2grpc-proto
+ */
+
+/**
+ * Path to the main MAVLink bridge proto definition file (mavlink_bridge.proto)
+ */
+export const protoPath: string;
+
+/**
+ * Directory containing all the MAVLink dialect proto definition files
+ */
+export const protoDir: string;
